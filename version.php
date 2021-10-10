a03bd09afbbd95ca6a63b375de7c7aa1cd8177a2
ponlawat-w <ponlawat_w@cmu.ac.th>
2021-10-10 13:07:59 +0000

    Fixed XMLDB Warning at itemid Default

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_forumdashboard';
-$plugin->version = 2021101005;
+$plugin->version = 2021101006;
 $plugin->requires = 2020061503;
 $plugin->depedencies = [
   'mod_forum' => 2020061501
